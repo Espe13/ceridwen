962b071a9ce60c574c3803c51b58314629e1fad4
Espe13 <42141549+Espe13@users.noreply.github.com>
2025-10-28 15:17:20 +0000

    DiffuseDust
    Add diffuse dust as a subclass of dust and update the init

--- ceridwen/dust/__init__.py
+++ ceridwen/dust/__init__.py
@@ -3,8 +3,8 @@ This module provides the Dust class for handling dust attenuation and emission i
 """
 
 # Import the Dust class from the corresponding file
-from .DustModel import Dust
+from .DustModel import Dust, DiffuseDust
 from .DustEmission import DustEmission
 
 # Define what gets imported with `from package import *`
-__all__ = ["Dust", "DustEmission", "attn_power_law", "attn_kriek_conroy"]
+__all__ = ["Dust", "DiffuseDust", "DustEmission", "attn_power_law", "attn_kriek_conroy"]
